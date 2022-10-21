7e1d7b15a4b19e6c0e710f28e0f547d7a5df09dc
Teyit <ajimamatovteyitbek@gmail.com>
2022-10-22 02:30:39 +0600

    replace aplication to remote

--- aplication.py
+++
@@ -1,8 +0,0 @@
-print("some code in python language")
-print("Empty")
-print("If use rebase -> return back to last commit place/time")
-print("diff -> show changes after commit (status-> detail)")
-print("git commit -am  -> add . & commit")
-
-
-
