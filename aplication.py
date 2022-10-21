e14d5598e231bc22d01384f09eb6caae2a2fc6fa
Teyit <ajimamatovteyitbek@gmail.com>
2022-10-22 02:35:08 +0600

    Show hash value

---
+++ aplication.py
@@ -0,0 +1,8 @@
+print("some code in python language")
+print("Empty")
+print("If use rebase -> return back to last commit place/time")
+print("diff -> show changes after commit (status-> detail)")
+print("git commit -am  -> add . & commit")
+print("Use hash value -> 57d6771d3d16bcde8c71ad18010c975a015502d4")
+
+
