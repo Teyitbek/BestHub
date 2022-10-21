e4663fd2271f015d4d7b94f6a427652769fe7c99
Teyit <ajimamatovteyitbek@gmail.com>
2022-10-22 01:56:54 +0600

    Refactofing

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 print("some code in python language")
 print("Empty")
+print("If use rebase -> return back to last commit place/time")
+print("diff -> show changes after commit (status-> detail)")
+
 
 
